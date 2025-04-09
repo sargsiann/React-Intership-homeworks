6f6670676fb6ff0926d4c007d93a7be54dad7268
sargsyandavid7 <sargsyand648@gmail.com>
2025-04-09 15:37:33 +0400

    implemented chaining basic , need all race allsetled any implementation and adding to microtasks

--- myPromise/script.js
+++ myPromise/script.js
@@ -20,49 +20,61 @@ function MyPromise(callback) {
     };
 
     this.then = function (cb) {
-        if (this.state === "fulfilled") {
-			let p = new MyPromise();	
-			try {
-				p.state = "fulfilled";
-				p.value = queueMicrotask(() => cb(this.value))
-			} catch (error) {
-				p.state = "rejected";
-				p.reason = error;
+		let reason = this.reason;
+		let value = this.value;
+        return new MyPromise((res,rej) => {
+			if (this.state === "rejected") {
+				rej(reason)
 			}
-			console.log(p);
-			return p;
-		}
+			else if (this.state === "fulfilled") {
+				try {
+					let v = cb && cb(value);
+					res(v);
+				} catch (error) {
+					rej(error);
+				}
+			}
+		})
     };
 
     this.catch = function (cb) {
-        if (this.state === "rejected") {
-			let p = new MyPromise();
-			try {
-				p.state = "fulfilled";
-				p.value = queueMicrotask(() => cb(this.value))
-			} catch (error) {
-				p.state = "rejected";
-				p.reason = error;
-			}	
-			return p;
-		}
+		let reason = this.reason;
+		let value = this.value;
+        return new MyPromise((res,rej) => {
+			if (this.state === "fulfilled") {
+				res(value);
+			}
+			else if (this.state === "rejected") {
+				try {
+					let v = cb && cb(reason);
+					res(v);
+				} catch (error) {
+					rej(error);
+				}
+			}
+		})
     };
 
     this.getState = function () {
         console.log(this.state, this.value, this.reason);
     };
 
-	this.finally = function() {
-		co
+	this.finally = function(cb) {
+		cb && queueMicrotask(cb);
 	}
 
 	if (callback)
-    	callback(this.resolve.bind(this), this.reject.bind(this));
+	{
+		try {
+			callback(this.resolve.bind(this), this.reject.bind(this));
+		} catch (error) {
+			this.reject(error);
+		}
+	}
 }
 
 let p = new MyPromise((res,rej) => {
-	res(3);
+	throw "adadd";
 });
 
-p.then(console.log)
-
+p.then(console.log).catch(console.log).then(console.log).finally()
