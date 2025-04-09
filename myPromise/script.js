b72700ffe4425852a536ca6f01a2ec95cd61f583
sargsyandavid7 <sargsyand648@gmail.com>
2025-04-09 16:17:45 +0400

    bad logic rewriting

--- myPromise/script.js
+++ myPromise/script.js
@@ -19,76 +19,12 @@ function MyPromise(callback) {
         }
     };
 
-    this.then = function (cb, cb1) {
-		let reason = this.reason;
-		let value = this.value;
-	
-		return new MyPromise((res, rej) => {
-			queueMicrotask(() => {
-				if (this.state === "rejected") {
-					if (cb1) {
-						try {
-							let v = cb1(reason);
-							res(v);
-						} catch (error) {
-							rej(error);
-						}
-					} else {
-						rej(reason);
-					}
-				} else if (this.state === "fulfilled") {
-					try {
-						let v = cb(value);
-						res(v);
-					} catch (error) {
-						rej(error);
-					}
-				}
-			});
-		});
-	}
-
-	this.catch = function (cb) {
-		let reason = this.reason;
-		let value = this.value;
-	
-		return new MyPromise((res, rej) => {
-			queueMicrotask(() => {
-				if (this.state === "fulfilled") {
-					res(value);
-				} else if (this.state === "rejected") {
-					try {
-						let v = cb && cb(reason);
-						res(v);
-					} catch (error) {
-						rej(error);
-					}
-				}
-			});
-		});
-	};
-	
-
-    this.getState = function () {
-        console.log(this.state, this.value, this.reason);
-    };
-
-	this.finally = function(cb) {
-		cb && queueMicrotask(cb);
-	}
-
-	if (callback)
-	{
-		try {
-			callback(this.resolve.bind(this), this.reject.bind(this));
-		} catch (error) {
-			this.reject(error);
-		}
-	}
 }
 
 let p = new MyPromise((res,rej) => {
 	throw "adadd";
 });
 
-p.then(console.log,console.log)
+let []
+
+console.log(p.then());
